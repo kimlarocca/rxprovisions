8df0fe416da554341806612a970519dd73736072
Kim LaRocca <kim@kimlarocca.com>
2020-12-18 12:01:03 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,188 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="l-container">
+    <v-spacer />
+    <v-header
+        layout="right"
+        :responsive=true
+    >
+      <template v-slot:logo>
+        <img
+            src="/images/logo.jpg"
+            width="150"
+            alt="GeneLeap Bioteach logo"
+        />
+      </template>
+      <template v-slot:button>
+        <v-button href="http://www.google.com">
+          Investors
+        </v-button>
+      </template>
+      <template v-slot:menu>
+        <v-menu
+            layout="right"
+            :primaryNav="[
+                    {
+                      url: '/about',
+                      text: 'About',
+                    },
+                    {
+                      url: '/technology',
+                      text: 'Technology',
+                    },
+                    {
+                      url: '/careers',
+                      text: 'Careers',
+                    },
+                    {
+                      url: '/news',
+                      text: 'News',
+                    },
+                    {
+                      url: '/contact',
+                      text: 'Contact',
+                    }
+                  ]"
+        >
+          <template v-slot:logo>
+            <img
+                src="https://place-hold.it/150x50?text=logo"
+                alt="logo"
+            />
+          </template>
+          <template v-slot:button>
+            <v-button href="http://www.google.com">
+              Click Me
+            </v-button>
+          </template>
+          <template v-slot:component>
+            <p class="u-align--center"><em>slot for component</em></p>
+          </template>
+          <template v-slot:social>
+            <ul class="social-links">
+              <social-link
+                  icon="fab fa-facebook-f"
+                  link="https://www.facebook.com"
+                  label="facebook"
+              >
+              </social-link>
+              <social-link
+                  icon="fab fa-twitter"
+                  link="https://www.twitter.com"
+                  label="twitter"
+              >
+              </social-link>
+              <social-link
+                  icon="fab fa-youtube"
+                  link="https://www.youtube.com"
+                  label="youtube"
+              >
+              </social-link>
+              <social-link
+                  icon="fab fa-instagram"
+                  link="https://www.instagram.com"
+                  label="instagram"
+              >
+              </social-link>
+              <social-link
+                  icon="fab fa-linkedin-in"
+                  link="https://www.linkedin.com"
+                  label="linkedin"
+              >
+              </social-link>
+            </ul>
+          </template>
+        </v-menu>
+      </template>
+      <template v-slot:navigation>
+        <secondary-navigation
+            orientation="horizontal"
+            :nav-items="[
+                    {
+                      url: '/about',
+                      text: 'About',
+                    },
+                    {
+                      url: '/technology',
+                      text: 'Technology',
+                    },
+                    {
+                      url: '/careers',
+                      text: 'Careers',
+                    },
+                    {
+                      url: '/news',
+                      text: 'News',
+                    },
+                    {
+                      url: '/contact',
+                      text: 'Contact',
+                    }
+                  ]"
+        />
+      </template>
+    </v-header>
+    <v-spacer size="hex" />
+    <router-view />
+    <v-spacer size="hex" />
   </div>
-  <router-view/>
+  <v-footer
+      company-name="GeneLeap Biotech"
+      :navigation="[
+                    {
+                      url: '/about',
+                      text: 'About',
+                    },
+                    {
+                      url: '/technology',
+                      text: 'Technology',
+                    },
+                    {
+                      url: '/careers',
+                      text: 'Careers',
+                    },
+                    {
+                      url: '/news',
+                      text: 'News',
+                    },
+                    {
+                      url: '/contact',
+                      text: 'Contact',
+                    },
+                    {
+                      url: '/privacy',
+                      text: 'Privacy',
+                    },
+                    {
+                      url: '/terms',
+                      text: 'Terms',
+                    }
+                  ]"
+  >
+    <template v-slot:blurb>
+      <p>Unless otherwise specified, all product names appearing on this Internet site are trademarks owned by or licensed to Luye Boston R&D IIC, its subsidiaries or affiliates. No use of any Luye Boston R&D IIC trademark, trade name, or trade dress on this site may be made without the prior written authorization of Luye Boston R&D IIC except to identify the product or services of the company. This Luye Boston R&D IIC website focusing on science is intended for the use of U.S. healthcare professionals any sciences.</p>
+    </template>
+  </v-footer>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SecondaryNavigation from 'vue-evolve/src/components/SecondaryNavigation'
+import SocialLink from 'vue-evolve/src/components/SocialLink'
+import VButton from 'vue-evolve/src/components/VButton'
+import VHeader from 'vue-evolve/src/components/VHeader'
+import VMenu from 'vue-evolve/src/components/VMenu'
+import VSpacer from 'vue-evolve/src/components/VSpacer'
+import VFooter from 'vue-evolve/src/components/VFooter'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'GeneLeap',
+  components: {
+    SecondaryNavigation,
+    SocialLink,
+    VButton,
+    VHeader,
+    VMenu,
+    VSpacer,
+    VFooter
   }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home l-container">
+    <div class="l-grid l-grid--2up l-grid--middle">
+      <h1>Completely Novel Gene Therapies</h1>
+      <p>GeneLeap Bioteach is a global gene therapy company in the Luye Lifesciences portfolio of companies, a leader in China and a new entrant in the US. Our goal is to deliver a best-in-class portfolio of completely novel gene therapies.</p>
+    </div>
+    <v-spacer size="quin" />
+    <div class="home-hero">
+      <div class="home-hero-circle" />
+      <img
+          src="/images/home-hero.jpg"
+          alt="GeneLeap Bioteach, a global gene therapy company"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VSpacer from 'vue-evolve/src/components/VSpacer'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    VSpacer
   }
 }
 </script>
+
+<style lang="scss">
+@import "src/assets/settings";
+.home {
+  position: relative;
+  &::after {
+    position: absolute;
+    top: 0;
+    right: -320px;
+    content: '';
+    height: 300px;
+    width: 300px;
+    border-radius: 50%;
+    background: var(--color-quaternary);
+    background: linear-gradient(180deg, var(--color-white) 0%, var(--color-tertiary) 50%, var(--color-tertiary) 100%);
+
+  }
+}
+.home .home-hero {
+  width: 100%;
+}
+
+.home .home-hero-circle {
+  position: relative;
+  height: 100px;
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 32px;
+    height: 300px;
+    width: 300px;
+    border-radius: 50%;
+    background: var(--color-primary);
+    background: linear-gradient(90deg, var(--color-quinary) 0%, var(--color-primary) 100%);
+    @media all and (min-width: $medium) {
+      left: 100px;
+      height: 400px;
+      width: 400px;
+    }
+  }
+}
+</style>
