163db84dd47f7aaf160e349214a3703e206c2d2a
Kim LaRocca <kim@kimlarocca.com>
2021-01-13 15:34:39 -0500

    barcode test

---
+++ src/views/BarcodeTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="u-padding">
+    <svg
+        class="barcode"
+        jsbarcode-format="CODE39"
+        jsbarcode-value="123456789012"
+        jsbarcode-textmargin="0"
+        jsbarcode-fontoptions="bold"
+    >
+    </svg>
+  </div>
+</template>
+
+<script>
+import JsBarcode from 'jsbarcode'
+
+export default {
+  mounted () {
+    JsBarcode('.barcode').init()
+  }
+}
+</script>
